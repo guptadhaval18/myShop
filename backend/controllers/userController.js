a7b4dd81ea92beab4f9dffb80880b821c0cbe0e7
guptadhaval18 <guptadhaval18@gmail.com>
2021-09-08 00:47:01 +0530

    authenticate middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,11 +24,56 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc     Register a new user
+// @route    POST /api/users
+// @sccess   Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
+
 // @desc     Get user profile
 // @route    GET /api/users/profile
 // @sccess   Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('Success')
-  })
+  const user = await User.findById(req.user._id)
 
-export { authUser, getUserProfile }
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, registerUser, getUserProfile }
